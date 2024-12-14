13141c61ae178ff0465f1dc3ca04c597e798ce9d
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2024-12-14 10:38:52 +0100

    feat: add CORS and logger middleware to server for improved request handling

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
 const hono = new Hono().basePath("/api/v1");
 
-hono.get("/hello", async (ctx) => {
-    return ctx.json({ message: "Hello, World!" });
-})
+// Global app utils middleware
+hono.use(cors());
+hono.use(logger());
 
 //TODO: Add all the routes here
+// hono.route('/auth', authRouter);good 
 
 export default hono;
